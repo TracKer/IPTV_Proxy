79f24d711adaad82ce3cf0ed867363b6c2cfaced
Andrey Rudenko <andrey.rudenko@gmail.com>
2020-01-09 02:14:42 +0200

    Update Source model, add helper methods

--- main/models/Source.py
+++ main/models/Source.py
@@ -1,11 +1,26 @@
+from datetime import datetime
+
 from django.db import models
+from django.utils.timezone import get_default_timezone
 
 
 class Source(models.Model):
     name = models.CharField(max_length=64, unique=True, null=False)
     url = models.CharField(max_length=255, null=False)
+    last_sequence = models.PositiveIntegerField()
+    target_duration = models.PositiveIntegerField()
     created = models.DateTimeField(auto_now_add=True)
     requested = models.DateTimeField(auto_now=True)
 
     class Meta:
         db_table = 'source'
+
+    def get_request_interval(self) -> int:
+        return round(self.target_duration / 2)
+
+    def is_outdated(self) -> bool:
+        # Outdated if last accessed >= (target_duration * 4)
+        tz = get_default_timezone()
+        now = datetime.now(tz)
+        interval = (now - self.requested).total_seconds()
+        return interval >= (self.target_duration * 4)
