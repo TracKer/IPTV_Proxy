0da375209b6a3d1d10219d206256fe68e6ad92a6
Andrey Rudenko <andrey.rudenko@gmail.com>
2020-01-09 23:27:35 +0200

    Update source and segment models

--- main/models/Segment.py
+++ main/models/Segment.py
@@ -4,11 +4,20 @@ from main.models import Source
 
 
 class Segment(models.Model):
+    _STATUSES = [
+        (0, 'New'),
+        (1, 'Available'),
+        (2, 'Watched')
+    ]
+
     source = models.ForeignKey(Source, on_delete=models.CASCADE, related_name='segments')
     duration = models.FloatField()
     name = models.CharField(max_length=255, null=False)
     name_original = models.CharField(max_length=255, null=False)
+    url = models.CharField(max_length=255, null=False)
+    status = models.PositiveSmallIntegerField(choices=_STATUSES, null=True, default=None)
     created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
 
     class Meta:
         db_table = 'segment'
--- main/models/Source.py
+++ main/models/Source.py
@@ -9,6 +9,7 @@ class Source(models.Model):
     url = models.CharField(max_length=255, null=False)
     last_sequence = models.PositiveIntegerField()
     target_duration = models.PositiveIntegerField()
+    segments_per_file = models.PositiveSmallIntegerField()
     created = models.DateTimeField(auto_now_add=True)
     requested = models.DateTimeField(auto_now=True)
 
@@ -16,11 +17,12 @@ class Source(models.Model):
         db_table = 'source'
 
     def get_request_interval(self) -> int:
+        # Request interval is a half of target_duration - average segment length
         return round(self.target_duration / 2)
 
     def is_outdated(self) -> bool:
-        # Outdated if last accessed >= (target_duration * 4)
+        # Outdated if no requests for (target_duration * segments_per_file) seconds - time of all segments in file
         tz = get_default_timezone()
         now = datetime.now(tz)
         interval = (now - self.requested).total_seconds()
-        return interval >= (self.target_duration * 4)
+        return interval >= (self.target_duration * self.segments_per_file)
