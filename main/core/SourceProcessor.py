cbc730b46fa584ff7ab804e3712a7fac975ba520
Andrey Rudenko <andrey.rudenko@gmail.com>
2020-01-19 23:41:07 +0200

    Mark properties as private

--- main/core/SourceProcessor.py
+++ main/core/SourceProcessor.py
@@ -1,22 +1,20 @@
+import m3u8
 from datetime import datetime
 from typing import List, Optional
-
-import m3u8
 from django.utils.timezone import get_default_timezone
 from m3u8 import M3U8, Playlist
-
 from main.models import Source
 
 
 class SourceProcessor:
     def __init__(self, url: Optional[str] = None, source: Optional[Source] = None) -> None:
         super().__init__()
-        self.url = url if url is not None else source.url
-        self.source = source if source is not None else self.__get_source()
+        self.__url = url if url is not None else source.url
+        self.__source = source if source is not None else self.__get_source()
 
     def __get_source(self) -> Source:
         try:
-            source = Source.objects.get(url=self.url)
+            source = Source.objects.get(url=self.__url)
         except Source.DoesNotExist:
             source = self.__get_source_from_external()
             source.save()
@@ -24,8 +22,8 @@ class SourceProcessor:
         return source
 
     def __get_source_from_external(self, source: Optional[Source] = None) -> Source:
-        m3u8_obj = self.__download_m3u8(self.url)
-        name = Source.generate_name(self.url)
+        m3u8_obj = self.__download_m3u8(self.__url)
+        name = Source.generate_name(self.__url)
 
         tz = get_default_timezone()
         now = datetime.now(tz)
@@ -33,7 +31,7 @@ class SourceProcessor:
         if source is None:
             source = Source()
         source.name = name
-        source.url = self.url
+        source.url = self.__url
         source.last_sequence = m3u8_obj.media_sequence
         source.target_duration = round(m3u8_obj.target_duration)
         source.segments_per_file = len(m3u8_obj.segments)
@@ -60,6 +58,6 @@ class SourceProcessor:
         return result
 
     def update_source(self, force: bool = False) -> None:
-        if force or self.source.is_data_outdated():
-            self.source = self.__get_source_from_external(self.source)
-            self.source.save()
+        if force or self.__source.is_data_outdated():
+            self.__source = self.__get_source_from_external(self.__source)
+            self.__source.save()
