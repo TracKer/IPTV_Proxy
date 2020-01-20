441cd156e56426d27ca7c6b3f19fbf7a4e894d60
Andrey Rudenko <andrey.rudenko@gmail.com>
2020-01-21 01:53:39 +0200

    Implement update source comannd for parallel processing

---
+++ main/management/commands/update_source.py
@@ -0,0 +1,21 @@
+from builtins import id
+
+from django.core.management import BaseCommand, CommandError
+from main.core.SourceProcessor import SourceProcessor
+from main.models import Source
+
+
+class Command(BaseCommand):
+    help = 'Update source'
+
+    def add_arguments(self, parser):
+        parser.add_argument('source_id', nargs=1, type=int)
+        pass
+
+    def handle(self, *args, **options):
+        source_id = options['source_id'][0]
+
+        source = Source.objects.get(id=source_id)
+
+        processor = SourceProcessor(source=source)
+        processor.update_source()
