e491037a9b761e579b5dfb7b286a88dd1290af20
Andrey Rudenko <andrey.rudenko@gmail.com>
2020-01-21 01:12:07 +0200

    Remove useless field

--- main/models/Segment.py
+++ main/models/Segment.py
@@ -24,7 +24,6 @@ class Segment(models.Model):
     duration = models.FloatField()
     name = models.CharField(max_length=255, null=False)
     name_original = models.CharField(max_length=255, null=False)
-    url = models.CharField(max_length=255, null=False)
     status = models.PositiveSmallIntegerField(choices=_STATUSES, null=True, default=None)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
